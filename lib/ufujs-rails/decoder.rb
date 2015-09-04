b742ea5559752b04ef3d284b0fecbdb4195113d0
Alexey-Lukin <lukin@active-bridge.com>
2015-09-04 11:05:19 +0300

    Add modify_values! method to hash and array

--- lib/ufujs-rails/decoder.rb
+++ lib/ufujs-rails/decoder.rb
@@ -1,41 +1,30 @@
 require 'base64'
 
-class Hash
-  def decode_base64_values
-    result = {}
-    self.map do |k, v|
-      result[k] = v.kind_of?(Hash) ? v.decode_base64_values : v
-      result[k] = v.map { |obj| obj.decode_base64_values if obj.kind_of?(Hash) } if v.kind_of?(Array)
-      result[k] = parse_image_data(result[k]) if base64_encoded?(result[k])
+class Array
+  def modify_values!
+    result = []
+    self.map do |obj|
+      result << if obj.kind_of?(Hash) && obj.kind_of?(Array)
+                  obj.modify_values!
+                else
+                  yield(obj)
+                end
     end
     result
   end
+end
 
-  private
-
-  def base64_encoded?(base64_image)
-    base64_image.present? && base64_image.is_a?(String) && base64_image.strip.include?("base64,")
-  end
-
-  def parse_image_data(base64_image)
-    filename = "upload-image" # FIXME we want the original name
-    in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
-
-    tempfile = Tempfile.new(filename)
-    tempfile.binmode
-    tempfile.write Base64.decode64(string)
-    tempfile.rewind
-
-    content_type = `file --mime -b #{tempfile.path}`.split(";")[0]
-
-    extension = content_type.match(/gif|jpeg|png/).to_s
-    filename += ".#{extension}" if extension
-
-    ActionDispatch::Http::UploadedFile.new({
-      tempfile: tempfile,
-      content_type: content_type,
-      filename: filename
-    })
+class Hash
+  def modify_values!
+    result = {}
+    self.map do |k, v|
+      result[k] = if v.kind_of?(Hash) && v.kind_of?(Array)
+                    v.modify_values!
+                  else
+                    yield(v)
+                  end
+    end
+    result
   end
 end
 
@@ -54,9 +43,35 @@ module Ufujs
 
     private
 
+    def base64_encoded?(base64_image)
+      base64_image.present? && base64_image.is_a?(String) && base64_image.strip.include?("base64,")
+    end
+
+    def parse_image_data(base64_image)
+      filename = "upload-image" # FIXME we want the original name
+      in_content_type, encoding, string = base64_image.split(/[:;,]/)[1..3]
+
+      tempfile = Tempfile.new(filename)
+      tempfile.binmode
+      tempfile.write Base64.decode64(string)
+      tempfile.rewind
+
+      content_type = `file --mime -b #{tempfile.path}`.split(";")[0]
+
+      extension = content_type.match(/gif|jpeg|png|jpg/).to_s
+      filename += ".#{extension}" if extension
+
+      ActionDispatch::Http::UploadedFile.new({
+        tempfile: tempfile,
+        content_type: content_type,
+        filename: filename
+      })
+    end
+
     def decoded_parameters(env)
       request = Rack::Request.new(env)
-      request.params.decode_base64_values
+      decode = Proc.new { |v| base64_encoded?(v) ? parse_image_data(v) : v }
+      request.params.modify_values!(&decode)
     end
   end
 end
